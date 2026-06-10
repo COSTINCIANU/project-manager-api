8bf9ef7fcb98434cb9af5663a33169bc5a1b757d
Costincianu Gheorghina <gheorghina.costincianu@sfr.fr>
2026-06-10 18:43:03 +0200

    ajout Sentry backend Symfony

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ['all' => true],
+    Sentry\SentryBundle\SentryBundle::class => ['prod' => true],
 ];
